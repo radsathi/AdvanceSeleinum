d27a2f1bc88fa00ad2305182463723ea5cfe0257
Admin <Admin@DESKTOP-G31GSKT>
2023-12-26 16:50:37 +0530

    pushproject

---
+++ com.crm.Advanceselenium/src/test/java/com/crm/Advanceselenium/TestcaseTest.java
@@ -0,0 +1,15 @@
+package com.crm.Advanceselenium;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestcaseTest {
+	
+@Test
+	public void test() {
+	
+		Reporter.log("project",true);
+		
+	}
+
+}
